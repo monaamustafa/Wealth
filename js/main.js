837806c8edc470a9ef9f415ab05a4f8d37fea87a
monaamustafa <monaamustafa7@gmail.com>
2021-07-21 21:30:32 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// let bookDetails=document.querySelectorAll('.bookDetails')
+// for(let i=0;i<bookDetails.length;i++){
+//   bookDetails[i].addEventListener('mouseover',function(){
+//     if(bookDetails[i].classList.contains('animate__shakeX')){
+//       bookDetails[i].classList.remove('animate__shakeX');
+//     bookDetails[i].classList.add('animate__shakeX');
+//     }
+//     else{
+      
+//     bookDetails[i].classList.add('animate__shakeX');
+//     }
+    
+//   })
+  
+// }
